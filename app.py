77c5ef67fbe77d49dd5ed8e563889bd063cc7914
alvsgabriellk <cauagabriell81@gmail.com>
2026-02-25 12:05:00 -0300

    Função de teste criada pra testar requisição de interpretação

--- app.py
+++ app.py
@@ -4,3 +4,16 @@ from interpretador import interpretar_mensagem
 app = Flask(__name__)
 
 
+@app.route("/whatsapp")
+def whatsapp():
+    msg = request.args.get("msg") # args = parâmetro ; get = pega valor pelo nome
+    if not msg:
+        return "Nenhuma mensagem enviada", 400 # dado errado ou faltando
+    resposta = interpretar_mensagem(msg)
+
+    return resposta
+
+
+
+if __name__ == "main":
+    app.run(debug=True)
