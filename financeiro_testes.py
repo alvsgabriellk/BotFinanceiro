33dd1048cb289169c4c7ee0efedd6f0ee564be63
alvsgabriellk <cauagabriell81@gmail.com>
2026-03-06 09:36:02 -0300

    Testando pra ver se tem bug

---
+++ financeiro_testes.py
@@ -0,0 +1,7 @@
+from modulos.financeiro import definirLimite, adicionarRendaExtra, definirSalario
+
+definirSalario("marco", 1500)
+adicionarRendaExtra("marco", 200)
+definirLimite("marco", 300)
+
+print("testes executados")
